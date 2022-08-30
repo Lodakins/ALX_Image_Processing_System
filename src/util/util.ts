a63d2725891adad402b40f5bc3e45de8f0b323db
lodakins <loadkins99@gmail.com>
2022-08-30 22:18:58 +0100

    fix: image filter function

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 import Jimp = require("jimp");
+import axios  from "axios";
+import { error } from "console";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,16 +13,26 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
-        });
+      axios({
+         method: 'get',
+         url: inputURL,
+        responseType: 'arraybuffer'
+      }).then( async function(response): Promise<void>{
+          const photo = await Jimp.read(response.data);
+          const outpath =
+            "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+           await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname + outpath, (img) => {
+              resolve(__dirname + outpath);
+            });
+        
+      }).catch(function(err){
+         reject(error);
+      })
+      
     } catch (error) {
       reject(error);
     }
